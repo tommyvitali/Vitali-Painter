6a53ccfc5d0d60d74a586dd158f23edb55e7509d
tommyvitali <56254943+tommyvitali@users.noreply.github.com>
2020-01-26 14:17:04 +0100

    26012020

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,23 +1,12 @@
-var piccola = document.getElementById("photoSmall");
+document.getElementById('check').onchange = function() {
+    document.getElementById('submit').disabled = !this.checked;
 
-var modal = document.getElementById("myModal");
-
-var modalImg = document.getElementById("img01");
-
-var close = document.getElementById("closeBtn");
-
-piccola.addEventListener("click", function() {
     
-    //cambio lo stile dello spazio modale cosi da farlo comparire
-    modal.style.display = "block"; //notare che cambio il display nello stile della ID e non della classe, che lo prende come conseguenza.
-    //annullo le ombre della foto piccola
-    modalImg.style.boxShadow = "none";
-    //nello spazio gli dico di prendere l'immagine di riferimento in QUESTA.
-    modalImg.src = this.src;
-})
+        
+    
+};
 
-close.addEventListener("click", function() {
 
-    modal.style.display = "none";
 
-})
+
+
