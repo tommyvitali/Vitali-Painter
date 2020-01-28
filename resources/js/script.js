4c0d28a4a298a4afd6a5423efd7a3159e5720473
tommyvitali <56254943+tommyvitali@users.noreply.github.com>
2020-01-28 18:25:01 +0100

    added darkmode

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,6 +7,68 @@ document.getElementById('check').onchange = function() {
 };
 
 
+function myFunction() {
+    var element = document.getElementById("body");
+    element.classList.toggle("body");
+
+    var element = document.getElementById("line");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line1");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line2");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line3");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line4");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line5");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line6");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line7");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line8");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line9");
+    element.classList.toggle("linewhite");
+    var element = document.getElementById("line10");
+    element.classList.toggle("linewhite");
+
+    var element = document.getElementById("details");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details1");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details2");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details3");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details4");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details5");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details6");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details7");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details8");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details9");
+    element.classList.toggle("detailswhite");
+    var element = document.getElementById("details10");
+    element.classList.toggle("detailswhite");
+
+    var element = document.getElementById("footer");
+    element.classList.toggle("footerwhite");
+
+    var element = document.getElementById("facebook");
+    element.classList.toggle("fa-facebookwhite");
+
+    var element = document.getElementById("instagram");
+    element.classList.toggle("fa-instagramwhite");
+
+    var element = document.getElementById("copyright");
+    element.classList.toggle("copyrightwhite");
+ }
 
 
 
